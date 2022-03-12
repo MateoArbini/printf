d2bea0e04eb63ed53f8d6f11a080c2ac15d7921d
MateoArbini <mateoarbini1998@gmail.com>
2022-03-12 15:29:07 -0800

    main con casos ok

--- main.c
+++ main.c
@@ -18,6 +18,12 @@ int main(void)
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
 
     return (0);
 }
