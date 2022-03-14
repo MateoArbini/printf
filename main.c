018ff61fbcfcd3aac8b5612e2bb89b154d0e50a9
MateoArbini <mateoarbini1998@gmail.com>
2022-03-14 00:10:30 -0700

    main with cases that are ok

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,6 +9,24 @@
  */
 int main(void)
 {
-    _printf("%b\n", 98);
+	int len;
+	int len2;
+	unsigned int ui;
+	ui = (unsigned int)INT_MAX + 1024;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len2);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
     return (0);
 }
