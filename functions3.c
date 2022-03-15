9e4feeda85e87e3d3168842d6966b7836f3c450f
MateoArbini <mateoarbini1998@gmail.com>
2022-03-15 16:52:10 -0700

    functions3.c

--- functions3.c
+++ functions3.c
@@ -20,7 +20,7 @@ void rec_hexa(unsigned int num)
 		if (num % 16 > 9 && num % 16 <= 16)
 		{
 			dif = (num % 16) - 9;
-			_putchar(dif + '@');
+			_putchar((dif + 1) + '_');
 		}
 		else
 			_putchar(num % 16 + '0');
@@ -30,7 +30,7 @@ void rec_hexa(unsigned int num)
 		if (num % 16 > 9 && num % 16 < 16)
 		{
 			dif = (num % 16) - 9;
-			_putchar(dif + '@');
+			_putchar((dif + 1) + '_');
 		}
 		else
 			_putchar(num % 16 + '0');
@@ -52,6 +52,58 @@ int print_hexa(va_list c)
 	iter = va_arg(c, unsigned int);
 
 	rec_hexa(iter);
+
+	for (num = 0; iter / 16; num++)
+	{
+		iter = iter / 16;
+	}
+	return (num + 1);
+}
+
+/**
+ *rec_HEXA - function that gives you the hexa number
+ *@num: user given number
+ **/
+void rec_HEXA(unsigned int num)
+{
+	int dif;
+
+	if (num / 16)
+	{
+	rec_HEXA(num / 16);
+		if (num % 16 > 9 && num % 16 <= 16)
+		{
+			dif = (num % 16) - 9;
+			_putchar(dif + '@');
+		}
+		else
+			_putchar(num % 16 + '0');
+	}
+	else
+	{
+		if (num % 16 > 9 && num % 16 < 16)
+		{
+			dif = (num % 16) - 9;
+			_putchar(dif + '@');
+		}
+		else
+			_putchar(num % 16 + '0');
+	}
+}
+
+/**
+ *print_HEXA - function that gives you the hexa number
+ *@c: alias of the list of parameters
+ *Return: num, which is the total amount of chars
+ **/
+int print_HEXA(va_list c)
+{
+	int num;
+	unsigned int iter;
+
+	iter = va_arg(c, unsigned int);
+
+	rec_HEXA(iter);
 
 	for (num = 0; iter / 16; num++)
 	{
